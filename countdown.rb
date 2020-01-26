e8e8b5eb62013cb81888c9d722bdfba9199e4c94
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-26 23:28:06 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,4 +6,11 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep(num)
+  while num > 0  
+  puts "#{num} SECOND(S)!"
+  num -= 1 
+  sleep 1 
+end
+"HAPPY NEW YEAR!"
+end
